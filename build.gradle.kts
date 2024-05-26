88ff470494ea28757745f2842f7f0183977f94cc
Baron Roberts <baron@cthing.com>
2024-05-26 10:47:50 -0700

    Migrate to cthing-projectversion.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,10 @@
 import com.github.spotbugs.snom.Confidence
 import com.github.spotbugs.snom.Effort
+import org.cthing.projectversion.BuildType
+import org.cthing.projectversion.ProjectVersion
 import java.text.SimpleDateFormat
-import java.util.*
+import java.util.Date
+import java.util.Locale
 
 repositories {
     mavenCentral()
@@ -18,12 +21,16 @@ plugins {
     alias(libs.plugins.versions)
 }
 
-val baseVersion = "1.0.1"
-val isSnapshot = true
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+    dependencies {
+        classpath(libs.cthingProjectVersion)
+    }
+}
 
-val isCIServer = System.getenv("CTHING_CI") != null
-val buildNumber = if (isCIServer) System.currentTimeMillis().toString() else "0"
-version = if (isSnapshot) "$baseVersion-$buildNumber" else baseVersion
+version = ProjectVersion("1.0.1", BuildType.snapshot)
 group = "org.cthing"
 description = "A library that writes JSON with or without pretty printing."
 
@@ -206,7 +213,8 @@ publishing {
         }
     }
 
-    val repoUrl = if (isSnapshot) findProperty("cthing.nexus.snapshotsUrl") else findProperty("cthing.nexus.candidatesUrl")
+    val repoUrl = if ((version as ProjectVersion).isSnapshotBuild)
+        findProperty("cthing.nexus.snapshotsUrl") else findProperty("cthing.nexus.candidatesUrl")
     if (repoUrl != null) {
         repositories {
             maven {
